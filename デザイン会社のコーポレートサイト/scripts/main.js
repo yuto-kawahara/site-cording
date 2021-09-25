ea75bbb08171fd8394f152eed02da3986e683a1c
yuto-kawahara <mashroom1384@gmail.com>
2021-09-26 08:47:45 +0900

    [Fix]PC版レイアウト

--- デザイン会社のコーポレートサイト/scripts/main.js
+++ デザイン会社のコーポレートサイト/scripts/main.js
@@ -1,4 +1,14 @@
-﻿document.addEventListener('DOMContentLoaded', function () {    
+﻿var header = document.querySelector('.header');
+
+
+document.addEventListener('DOMContentLoaded', function () {    
   // const main = new Main();
   new MobileMenu();
+  new ScrollObserver('.header', function(el, inview){
+    if(inview) {
+      header.classList.remove('triggered');
+    } else {
+      header.classList.add('triggered');
+    }
+  }, {once: false});
 });
--- デザイン会社のコーポレートサイト/scripts/mobile-menu.js
+++ デザイン会社のコーポレートサイト/scripts/mobile-menu.js
@@ -1,22 +1,22 @@
 ﻿class MobileMenu {
-    constructor() {
-        this.DOM = {};
-        this.DOM.btn = document.querySelector('.mobile-menu__btn');
-        this.DOM.nav = document.querySelector('.header__nav');
-        this.eventType = this._getEventType();
-        this._addEvent();
-    }
+  constructor() {
+    this.DOM = {};
+    this.DOM.btn = document.querySelector(".mobile-menu__btn");
+    this.DOM.nav = document.querySelector(".header__nav");
+    this.eventType = this._getEventType();
+    this._addEvent();
+  }
 
-    _getEventType() {
-        return window.ontouchstart ? 'touchstart' : 'click';
-    }
+  _getEventType() {
+    return window.ontouchstart ? "touchstart" : "click";
+  }
 
-    _toggle() {
-        this.DOM.nav.classList.toggle('menu-open');
-        this.DOM.btn.classList.toggle('menu-open');
-    }
+  _toggle() {
+    this.DOM.nav.classList.toggle("menu-open");
+    this.DOM.btn.classList.toggle("menu-open");
+  }
 
-    _addEvent() {
-        this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
-    }
+  _addEvent() {
+    this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
+  }
 }
