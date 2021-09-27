45eeb1417a92cb9fb2bdf07523d618ca51ba4f97
yuto-kawahara <mashroom1384@gmail.com>
2021-09-27 16:55:41 +0900

    スクロール検知によるヘッダーの可視化

--- デザイン会社のコーポレートサイト(上級編)/js/main.js
+++ デザイン会社のコーポレートサイト(上級編)/js/main.js
@@ -1,5 +1,32 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {    
-  // const main = new Main();
+  const main = new Main();
   new MobileMenu();
 
 });
+
+class Main {
+  constructor() {
+    this.header = document.querySelector('.header');
+    this._observers = [];
+    this._srollInit();
+  }
+  set observers(val) {
+    this._observers.push(val);
+  }
+  get observers() {
+    return this._observers;
+  }
+
+  _headerTrigger(el, inview) {
+    if (inview) {
+      this.header.classList.remove('triggered');
+    }
+    else {
+      this.header.classList.add('triggered');
+    }
+  }
+
+  _srollInit() {
+    this.observers = new ScrollObserver('.top-visual', this._headerTrigger.bind(this), {once: false});
+  }
+}
---
+++ デザイン会社のコーポレートサイト(上級編)/js/scroll.js
@@ -0,0 +1,47 @@
+﻿class ScrollObserver  {
+  // コンストラクタ
+  constructor(els, cb, options) {
+    // 要素を取得
+    this.els = document.querySelectorAll(els);
+    // ScrollObserverの初期設定
+    const defaultOptions = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0,
+      once: true
+    };
+    // コールバック関数を設定
+    this.cb = cb;
+    // optionsの設定をdefault設定に上書き
+    this.options = Object.assign(defaultOptions, options)
+    // スクロール検知を1回で監視停止
+    this.once= this.options.once;
+    this._init();
+  }
+  // 初期化関数
+  _init() {
+    // 要素の交差を監視するコールバック関数
+    const callback = function(entries, observer) {
+      entries.forEach(entry => {
+        // 要素が監視中
+        if (entry.isIntersecting) {
+          // cbに登録された関数を実行
+          this.cb(entry.target, true);
+          // スクロール検知を1回で監視停止
+          if (this.once){
+            observer.unobserve(entry.target);
+          }
+        }
+        else {
+          // cbに登録された関数を実行
+          this.cb(entry.target, false);
+        }
+      });
+    };
+
+    // IntersectionObserverにコールバック関数とオプションを渡す
+    this.io = new IntersectionObserver(callback.bind(this), this.options)
+    // 要素の監視を開始
+    this.els.forEach(el => this.io.observe(el));
+  };
+}
